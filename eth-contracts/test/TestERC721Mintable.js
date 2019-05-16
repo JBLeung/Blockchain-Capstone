b31b0e98cfa6efc6e39b3feebc3b6cd2dd5b7f39
JBLeung <jbleunghk@gmail.com>
2019-05-16 21:06:08 +0800

    fix eslint for TestERC721Mintable.js

--- eth-contracts/test/TestERC721Mintable.js
+++ eth-contracts/test/TestERC721Mintable.js
@@ -1,47 +1,45 @@
-var ERC721MintableComplete = artifacts.require('ERC721MintableComplete');
+const ERC721MintableComplete = artifacts.require('ERC721MintableComplete')
 
-contract('TestERC721Mintable', accounts => {
+contract('TestERC721Mintable', (accounts) => {
+  const account_one = accounts[0]
+  const account_two = accounts[1]
 
-    const account_one = accounts[0];
-    const account_two = accounts[1];
+  describe('match erc721 spec', () => {
+    beforeEach(async function () {
+      this.contract = await ERC721MintableComplete.new({from: account_one})
 
-    describe('match erc721 spec', function () {
-        beforeEach(async function () { 
-            this.contract = await ERC721MintableComplete.new({from: account_one});
+      // TODO: mint multiple tokens
+    })
 
-            // TODO: mint multiple tokens
-        })
+    it('should return total supply', async () => {
 
-        it('should return total supply', async function () { 
-            
-        })
+    })
 
-        it('should get token balance', async function () { 
-            
-        })
+    it('should get token balance', async () => {
 
-        // token uri should be complete i.e: https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/1
-        it('should return token uri', async function () { 
-            
-        })
+    })
 
-        it('should transfer token from one owner to another', async function () { 
-            
-        })
-    });
+    // token uri should be complete i.e: https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/1
+    it('should return token uri', async () => {
 
-    describe('have ownership properties', function () {
-        beforeEach(async function () { 
-            this.contract = await ERC721MintableComplete.new({from: account_one});
-        })
+    })
 
-        it('should fail when minting when address is not contract owner', async function () { 
-            
-        })
+    it('should transfer token from one owner to another', async () => {
 
-        it('should return contract owner', async function () { 
-            
-        })
+    })
+  })
 
-    });
+  describe('have ownership properties', () => {
+    beforeEach(async function () {
+      this.contract = await ERC721MintableComplete.new({from: account_one})
+    })
+
+    it('should fail when minting when address is not contract owner', async () => {
+
+    })
+
+    it('should return contract owner', async () => {
+
+    })
+  })
 })
